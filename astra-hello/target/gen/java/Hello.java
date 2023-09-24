e3c89c141ce8572839782736ea8835572e69762f
Seán Kendall <sean.kendall@ucdconnect.ie>
2023-09-24 13:23:37 +0100

    Adding initial test runs for Maven and Astra

---
+++ astra-hello/target/gen/java/Hello.java
@@ -0,0 +1,83 @@
+/**
+ * GENERATED CODE - DO NOT CHANGE
+ */
+
+import astra.core.*;
+import astra.execution.*;
+import astra.event.*;
+import astra.messaging.*;
+import astra.formula.*;
+import astra.lang.*;
+import astra.statement.*;
+import astra.term.*;
+import astra.type.*;
+import astra.tr.*;
+import astra.reasoner.util.*;
+
+
+public class Hello extends ASTRAClass {
+	public Hello() {
+		setParents(new Class[] {astra.lang.Agent.class});
+		addRule(new Rule(
+			"Hello", new int[] {4,9,4,28},
+			new GoalEvent('+',
+				new Goal(
+					new Predicate("main", new Term[] {
+						new Variable(Type.LIST, "args",false)
+					})
+				)
+			),
+			Predicate.TRUE,
+			new Block(
+				"Hello", new int[] {4,27,6,5},
+				new Statement[] {
+					new ModuleCall("console",
+						"Hello", new int[] {5,4,5,37},
+						new Predicate("println", new Term[] {
+							Primitive.newPrimitive("Hello, World! ")
+						}),
+						new DefaultModuleCallAdaptor() {
+							public boolean inline() {
+								return true;
+							}
+
+							public boolean invoke(Intention intention, Predicate predicate) {
+								return ((astra.lang.Console) intention.getModule("Hello","console")).println(
+									(java.lang.String) intention.evaluate(predicate.getTerm(0))
+								);
+							}
+						}
+					)
+				}
+			)
+		));
+	}
+
+	public void initialize(astra.core.Agent agent) {
+	}
+
+	public Fragment createFragment(astra.core.Agent agent) throws ASTRAClassNotFoundException {
+		Fragment fragment = new Fragment(this);
+		fragment.addModule("console",astra.lang.Console.class,agent);
+		return fragment;
+	}
+
+	public static void main(String[] args) {
+		Scheduler.setStrategy(new TestSchedulerStrategy());
+		ListTerm argList = new ListTerm();
+		for (String arg: args) {
+			argList.add(Primitive.newPrimitive(arg));
+		}
+
+		String name = java.lang.System.getProperty("astra.name", "main");
+		try {
+			astra.core.Agent agent = new Hello().newInstance(name);
+			agent.initialize(new Goal(new Predicate("main", new Term[] { argList })));
+			Scheduler.schedule(agent);
+		} catch (AgentCreationException e) {
+			e.printStackTrace();
+		} catch (ASTRAClassNotFoundException e) {
+			e.printStackTrace();
+		};
+	}
+}
